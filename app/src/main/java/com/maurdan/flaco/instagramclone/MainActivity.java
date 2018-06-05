690449f89894392cf871e628ca89796e9d8042bd
Daniel Dawson <dannyldawson@gmail.com>
2018-06-04 20:27:51 -0700

    Alternate signup and login modes

--- app/src/main/java/com/maurdan/flaco/instagramclone/MainActivity.java
+++ app/src/main/java/com/maurdan/flaco/instagramclone/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
@@ -12,14 +14,39 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Boolean signUpModeActive = true;
+    TextView loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginTextView = findViewById(R.id.loginTextView);
+        loginTextView.setOnClickListener(this);
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.loginTextView) {
+
+            Button signUpButton = findViewById(R.id.signUpButton);
+
+            if (signUpModeActive) {
+                signUpModeActive = false;
+                signUpButton.setText("Login");
+                loginTextView.setText("Signup");
+            } else {
+                signUpModeActive = true;
+                signUpButton.setText("Signup");
+                loginTextView.setText("Login");
+            }
+
+        }
     }
 
     public void signUpClicked(View view) {
