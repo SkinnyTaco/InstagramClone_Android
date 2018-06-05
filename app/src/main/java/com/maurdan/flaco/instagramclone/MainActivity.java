bf577ef2a419831e6e57ecff3216088e18f4d25f
Daniel Dawson <dannyldawson@gmail.com>
2018-06-04 20:10:28 -0700

    Added signup button functionality

--- app/src/main/java/com/maurdan/flaco/instagramclone/MainActivity.java
+++ app/src/main/java/com/maurdan/flaco/instagramclone/MainActivity.java
@@ -2,10 +2,15 @@ package com.maurdan.flaco.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void signUpClicked(View view) {
         EditText userNameEditText = findViewById(R.id.userNameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+        if (userNameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
+            Toast.makeText(this, "A username and a password are required", Toast.LENGTH_SHORT).show();
+        } else {
+            ParseUser user = new ParseUser();
+            user.setUsername(userNameEditText.getText().toString());
+            user.setPassword(passwordEditText.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Log.i("Signup", "Success");
+                    } else {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
 }
--- app/src/main/java/com/maurdan/flaco/instagramclone/Parser.java
+++ app/src/main/java/com/maurdan/flaco/instagramclone/Parser.java
@@ -3,15 +3,29 @@ package com.maurdan.flaco.instagramclone;
 import com.parse.Parse;
 import android.app.Application;
 
+
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
 public class Parser extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+//        Parse.enableLocalDatastore(this);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("659cf036f5aad296ab1fd61010b43a1d3d3f5094")
                 .clientKey("9d0d1a7f28bef10e05d1065b2570c8576c85e559")
                 .server("http://13.58.186.216:80/parse")
                 .build()
         );
+
+//        ParseUser.enableAutomaticUser();
+//
+//        ParseACL defaultACL = new ParseACL();
+//        defaultACL.setPublicReadAccess(true);
+//        defaultACL.setPublicWriteAccess(true);
+//        ParseACL.setDefaultACL(defaultACL, true);
     }
 }
